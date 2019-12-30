9703e1b64d37e613b6b2d7a409da8019635d8737
Sam Humphries <samuel.humphries@westpoint.edu>
2019-12-30 02:14:46 +0000

    U-Net round 2

--- data.py
+++ data.py
@@ -84,7 +84,7 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
 
 def testGenerator(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
     for i in range(num_image):
-        img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
+        img = io.imread(os.path.join(test_path,"PAN_AOI_2_Vegas_img1005.png"%i),as_gray = as_gray)
         img = img / 255
         img = trans.resize(img,target_size)
         img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
